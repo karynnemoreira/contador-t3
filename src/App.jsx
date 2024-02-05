42d777d1897dc5f83aa77a3c2b7562580acac253
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-02-05 19:35:52 -0300

    contador t3

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react" //importando o react
+import Contador from "./Contador/Contador.jsx" //importando o contador.jsx, de dentro da pasta Contador
+
+function App(){
+  return(
+    <> 
+    <Contador />
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Contador/Contador.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Contador() {
+    return (
+        <>
+            <h1>0</h1>
+            <button>+</button>
+            <button>-</button>
+            <button>C</button>
+        </>
+    )
+}
+
+export default Contador
